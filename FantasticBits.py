bec68c0ae11871afbfc8b49e5639077b103ac8f0
Beramos <bram.de.jaegher@gmail.com>
2017-01-10 21:56:27 +0100

    minimal working script

--- FantasticBits.py
+++ FantasticBits.py
@@ -6,7 +6,13 @@ import math
 
 my_team_id = int(input())  # if 0 you need to score on the right of the map, if 1 you need to score on the left
 
-# Initiate lists for data storage
+# initiate center of goals
+if my_team_id is 0:
+    goalCenter=(0, 3750)
+else:
+    goalCenter=(16000, 3750)
+
+# initiate lists for data storage
 entity_idList=[]
 entity_typeList=[]
 xList=[]
@@ -39,8 +45,8 @@ while True:
     my_score, my_magic = [int(i) for i in input().split()]
     opponent_score, opponent_magic = [int(i) for i in input().split()]
     entities = int(input())  # number of entities still in game
-    snaffleList=list() # list of ID's for the snaffles
-    wizardList=list()           # list of ID's for my wizards
+    SnaffleList=list() # list of ID's for the snaffles
+    WizardList=list()           # list of ID's for my wizards
 
     for i in range(entities):
         # entity_id: entity identifier
@@ -52,22 +58,22 @@ while True:
         # state: 1 if the wizard is holding a Snaffle, 0 otherwise
         entity_id, entity_type, x, y, vx, vy, state = input().split()
 
-        if entity_type == "WIZARD"
-            wizardList.add(WizardClass(x,y,vx,vy,state))
+        if entity_type == "WIZARD":
+            WizardList.append(WizardClass(int(x),int(y),int(vx),int(vy),int(state)))
 
-        elif entity_type == "SNAFFLE"
-            snaffleID.add(i)
+        elif entity_type == "SNAFFLE":
+            SnaffleList.append(SnaffleClass(int(x),int(y),int(vx),int(vy),int(state)))
 
     for i in range(2):
+        wizard=WizardList[i]
 
-        wizard=wizardList[i]
+        if wizard.state is 1:
+            print("THROW" + " " + str(goalCenter[1]) + " " + str(goalCenter[2]) + " " + "500")
+        else:
+            print("MOVE 8000 3750 100")
 
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr)
 
-
         # Edit this line to indicate the action for each wizard (0 ≤ thrust ≤ 150, 0 ≤ power ≤ 500)
         # i.e.: "MOVE x y thrust" or "THROW x y power"
-        if state[wizardID[]]
-
-        print("MOVE 8000 3750 100")
