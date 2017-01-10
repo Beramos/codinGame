88fc9756b0227f0cdbf283cb7ac2b95f72d33ba5
Beramos <bram.de.jaegher@gmail.com>
2017-01-10 20:23:21 +0100

    initial script

---
+++ FantasticBits.py
@@ -0,0 +1,39 @@
+import sys
+import math
+
+# Grab Snaffles and try to throw them through the opponent's goal!
+# Move towards a Snaffle and use your team id to determine where you need to throw it.
+
+my_team_id = int(input())  # if 0 you need to score on the right of the map, if 1 you need to score on the left
+
+# game loop
+while True:
+    my_score, my_magic = [int(i) for i in input().split()]
+    opponent_score, opponent_magic = [int(i) for i in input().split()]
+    entities = int(input())  # number of entities still in game
+    for i in range(entities):
+        # entity_id: entity identifier
+        # entity_type: "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
+        # x: position
+        # y: position
+        # vx: velocity
+        # vy: velocity
+        # state: 1 if the wizard is holding a Snaffle, 0 otherwise
+        entity_id, entity_type, x, y, vx, vy, state = input().split()
+        entity_id = int(entity_id)
+        x = int(x)
+        y = int(y)
+        vx = int(vx)
+        vy = int(vy)
+        state = int(state)
+    for i in range(2):
+
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr)
+
+
+        # Edit this line to indicate the action for each wizard (0 ≤ thrust ≤ 150, 0 ≤ power ≤ 500)
+        # i.e.: "MOVE x y thrust" or "THROW x y power"
+        if
+
+        print("MOVE 8000 3750 100")
