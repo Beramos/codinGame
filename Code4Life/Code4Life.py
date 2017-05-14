6c2a249caca39469a754d0a6dee67e9876de17b7
Bram <bram.dejaegher@ugent.be>
2017-05-14 17:07:04 +0200

    fixing some errors

--- Code4Life/Code4Life.py
+++ Code4Life/Code4Life.py
@@ -12,33 +12,41 @@ class myBot(object):
         self.id_or_type = ""
         self.capacity = [3, 10]          # carrying capacity 0:data,1:MOLECULES
         self.storage = [0, 0, 0, 0, 0]
-        self.my_recipe_ids = []
-        self.cloud_recipe_ids = []
+        self.my_recipes = []
+        self.cloud_recipes = []
         self.queue = []
         # self.expertise = expertise
         self.sample_list = []
-        self.ranked_recipes = []
+        self.ranked_recipes_cloud = []
+        self.ranked_recipes_my = []
+        self.my_recipe_ids = []
+        self.cloud_recipe_ids = []
 
     def DIAGNOSIS_berserk(self):
-        self.ranked_recipes = rank_recipes(self.sample_list)
+        self.ranked_recipes_cloud = rank_recipes(self.cloud_recipes)
         for i in range(1,self.num_open_slots("data",self.capacity,self.my_recipe_ids,self.storage)):
-            self.queue.append("CONNECT " + str(self.ranked_recipes[i]['id']))
-        return "CONNECT " + str(self.ranked_recipes[0]['id'])
+            self.queue.append("CONNECT " + str(self.ranked_recipes_cloud[i]['id']))
+        return "CONNECT " + str(self.ranked_recipes_cloud[0]['id'])
 
     def MOLECULAR_madness(self):        # Horrible implementation
-        self.ranked_recipes = rank_recipes(self.sample_list)
-        self.can_complete = 0
-        mol1 = needed_molecules(self.ranked_recipes[0]['cost'],self.storage)
-        mol2 = needed_molecules(self.ranked_recipes[1]['cost'],[0, 0, 0, 0, 0])
-        mol3 = needed_molecules(self.ranked_recipes[2]['cost'],[0, 0, 0, 0, 0])
+        print(self.ranked_recipes_my, file = sys.stderr)
+        self.can_complete = -1
+        mol1 = needed_molecules(self.ranked_recipes_my[0]['cost'],self.storage)
+        mol2 = needed_molecules(self.ranked_recipes_my[1]['cost'],[0, 0, 0, 0, 0])
+        mol3 = needed_molecules(self.ranked_recipes_my[2]['cost'],[0, 0, 0, 0, 0])
         tempInv = [mol1[i] + self.storage[i] for i in range(0,len(self.storage))]
-        molTot = mol1
+
+        if self.num_open_slots("molecules",self.capacity,self.my_recipe_ids,mol1) >= sum(mol1):
+            molTot = mol1
+            self.can_complete = 0
 
         if self.num_open_slots("molecules",self.capacity,self.my_recipe_ids,molTot) >= sum(mol2):
             molTot = [mol1[i] + mol2[i] for i in range(0,5)]
+            self.can_complete = 1
 
         if self.num_open_slots("molecules",self.capacity,self.my_recipe_ids,molTot) >= sum(mol3):
             molTot = [mol1[i] + mol2[i] +mol3[i] for i in range(0,5)]
+            self.can_complete = 2
 
         for i in range(0, molTot[0]):
             self.queue.append('CONNECT ' + 'A')
@@ -65,18 +73,21 @@ class myBot(object):
 
     def LABORATORY_lazarus(self):
         if self.can_complete >= 1:
-            self.queue.append('CONNECT ' + self.ranked_recipes[1]['id'])
+            self.queue.append('CONNECT ' + str(self.ranked_recipes_my[1]['id']))
+            print('I can make two meds.',file = sys.stderr)
         if self.can_complete >= 2:
-            self.queue.append('CONNECT ' + self.ranked_recipes[2]['id'])
+            self.queue.append('CONNECT ' + str(self.ranked_recipes_my[2]['id']))
+            print('I can make three meds',file = sys.stderr)
         if self.can_complete >= 0:
-            return 'CONNECT ' + str(self.ranked_recipes[0]['id'])
+            print('I can make one meds.',file = sys.stderr)
+            return 'CONNECT ' + str(self.ranked_recipes_my[0]['id'])
 
     def exe_queue(self):
         return self.queue.pop(0)
 
     def can_make_med(self): # needs revision ---------------------------!
-        if len(self.ranked_recipes) > 0:
-            entry = self.ranked_recipes[0]
+        if len(self.ranked_recipes_my) > 0:
+            entry = self.ranked_recipes_my[0]
             if (min([entry['cost'][i]-self.storage[i] for i in range(0,len(entry['cost']))]) <= 0) & \
                 (sum([entry['cost'][i]-self.storage[i] for i in range(0,len(entry['cost']))]) <= 0):
                 return True
@@ -96,11 +107,17 @@ class myBot(object):
             return "GOTO " + self.destination
 
     def update_recipes(self):
+        self.my_recipes = []
+        self.cloud_recipes = []
         self.my_recipe_ids = []
+        self.cloud_recipe_ids = []
+
         for entry in self.sample_list:
             if entry['carrier'] == 0:
+                self.my_recipes.append(entry)
                 self.my_recipe_ids.append(entry['id'])
             elif entry['carrier'] == -1:
+                self.cloud_recipes.append(entry)
                 self.cloud_recipe_ids.append(entry['id'])
         return True
 
@@ -118,6 +135,8 @@ class myBot(object):
             return True
         else:
             if max(health_cloud_recipes) > max(health_my_recipes):
+                print(health_cloud_recipes + health_my_recipes, file = sys.stderr)
+                print(self.sample_list, file = sys.stderr)
                 return True
             else:
                 return False
@@ -142,14 +161,14 @@ def rank_recipes(recipes):
     copy_recipes = recipes
     max_health = -1
     ranked_recipes = []
-    for i in range(0,3):
+    for i in range(0,min([len(recipes)]+[3])):
         counter = 0
         for recipe in copy_recipes:
             if (recipe['carrier'] == -1) & (recipe['health'] > max_health):  # if it is in the cloud
                 max_health = recipe['health']
                 id_best_recipe = recipe['id']
-                counter += 1
             ranked_recipes.append(copy_recipes.pop(counter))
+            counter += 1
     return ranked_recipes
 
 def needed_molecules(recipe,inventory):
@@ -161,6 +180,8 @@ def roche_Fort_10(storage,expertise,sample_list,MyBot):
     MyBot.sample_list = sample_list #update sample_list
     MyBot.update_recipes()          #distinguish between cloud recipes and my recipes
     MyBot.storage = storage
+    MyBot.ranked_recipes_my = rank_recipes(MyBot.my_recipes)
+    MyBot.ranked_recipes_cloud = rank_recipes(MyBot.cloud_recipes)
 
     if len(MyBot.queue) > 0:
         move = MyBot.exe_queue()
@@ -179,7 +200,6 @@ def roche_Fort_10(storage,expertise,sample_list,MyBot):
 
     print(move)
     return True
-
 
 # Initiate step counter
 step_counter = 0
